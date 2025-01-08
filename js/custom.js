2509b4e73f80158f4fb6d4135ffeccad9a7733ae
raouf-allala <96212241+raouf-allala@users.noreply.github.com>
2025-01-08 23:18:58 +0100

    fixed videos loop

--- js/custom.js
+++ js/custom.js
@@ -28,33 +28,30 @@
 // Table of Contents End
 // ------------------------------------------------
 
-$(window).on("load", function() {
-   
-  "use strict";
+$(window).on('load', function () {
+  'use strict';
 
   // --------------------------------------------- //
   // Loader & Loading Animation Start
   // --------------------------------------------- //
-  $(".loader__logo").addClass('scaleOut');
+  $('.loader__logo').addClass('scaleOut');
 
-  setTimeout(function(){
-    $(".loader").addClass('loaded');
-    $("#main").addClass('active animate-in');
+  setTimeout(function () {
+    $('.loader').addClass('loaded');
+    $('#main').addClass('active animate-in');
     $('#home-trigger').addClass('active-link');
   }, 300);
- 
-  setTimeout(function(){
-    $("body").addClass('loaded');
+
+  setTimeout(function () {
+    $('body').addClass('loaded');
   }, 950);
   // --------------------------------------------- //
   // Loader & Loading Animation End
   // --------------------------------------------- //
-
 });
 
-$(function() {
-
-  "use strict";
+$(function () {
+  'use strict';
 
   // --------------------------------------------- //
   // Swiper Slider Start
@@ -62,35 +59,34 @@ $(function() {
   var swiper = new Swiper('.swiper', {
     // Optional parameters
     grabCursor: true,
-    effect: "creative",
+    effect: 'creative',
     creativeEffect: {
       prev: {
-        translate: ["-20%", 0, -1],
+        translate: ['-20%', 0, -1],
       },
       next: {
-        translate: ["100%", 0, 0],
+        translate: ['100%', 0, 0],
       },
     },
     parallax: true,
     speed: 1300,
     loop: true,
     autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
+      delay: 5000,
+      disableOnInteraction: true,
     },
-  
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
   });
   // --------------------------------------------- //
   // Swiper Slider End
@@ -112,28 +108,36 @@ $(function() {
   // Magnific Popup Video Start
   // --------------------------------------------- //
   $('#inner-video-trigger').magnificPopup({
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
     fixedContentPos: false,
     callbacks: {
-      beforeOpen: function() { $('body').addClass('overflow-hidden'); },
-      close: function() { $('body').removeClass('overflow-hidden'); }
-    }
-	});
+      beforeOpen: function () {
+        $('body').addClass('overflow-hidden');
+      },
+      close: function () {
+        $('body').removeClass('overflow-hidden');
+      },
+    },
+  });
 
   $('#showreel-trigger').magnificPopup({
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
     fixedContentPos: false,
     callbacks: {
-      beforeOpen: function() { $('body').addClass('overflow-hidden'); },
-      close: function() { $('body').removeClass('overflow-hidden'); }
-    }
-	});
+      beforeOpen: function () {
+        $('body').addClass('overflow-hidden');
+      },
+      close: function () {
+        $('body').removeClass('overflow-hidden');
+      },
+    },
+  });
   // --------------------------------------------- //
   // Magnific Popup Video End
   // --------------------------------------------- //
@@ -141,8 +145,14 @@ $(function() {
   // --------------------------------------------- //
   // KBW-Countdown Start
   // --------------------------------------------- //
-  $('#countdown').countdown({until: $.countdown.UTCDate(+10, 2024, 5, 27), format: 'D'});
-  $('#countdownLine').countdown({until: $.countdown.UTCDate(+10, 2024, 5, 27), format: 'D'});
+  $('#countdown').countdown({
+    until: $.countdown.UTCDate(+10, 2024, 5, 27),
+    format: 'D',
+  });
+  $('#countdownLine').countdown({
+    until: $.countdown.UTCDate(+10, 2024, 5, 27),
+    format: 'D',
+  });
   // --------------------------------------------- //
   // KBW-Countdown End
   // --------------------------------------------- //
@@ -151,18 +161,23 @@ $(function() {
   // Vegas Kenburns Start
   // --------------------------------------------- //
   var bgndKenburns = $('#bgndKenburns');
-  if(bgndKenburns.length){
+  if (bgndKenburns.length) {
     bgndKenburns.vegas({
       timer: false,
       delay: 8000,
       transition: 'fade2',
       transitionDuration: 2000,
       slides: [
-        { src: "https://dummyimage.com/1000x1500/4d4d4d/636363" },
-        { src: "https://dummyimage.com/1000x1500/4d4d4d/636363" },
-        { src: "https://dummyimage.com/1000x1500/4d4d4d/636363" }
+        { src: 'https://dummyimage.com/1000x1500/4d4d4d/636363' },
+        { src: 'https://dummyimage.com/1000x1500/4d4d4d/636363' },
+        { src: 'https://dummyimage.com/1000x1500/4d4d4d/636363' },
       ],
-      animation: [ 'kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight' ]
+      animation: [
+        'kenburnsUp',
+        'kenburnsDown',
+        'kenburnsLeft',
+        'kenburnsRight',
+      ],
     });
   }
   // --------------------------------------------- //
@@ -174,30 +189,40 @@ $(function() {
   // --------------------------------------------- //
   $('.notify-form').ajaxChimp({
     callback: mailchimpCallback,
-    url: 'https://besaba.us10.list-manage.com/subscribe/post?u=e8d650c0df90e716c22ae4778&amp;id=54a7906900'
+    url: 'https://besaba.us10.list-manage.com/subscribe/post?u=e8d650c0df90e716c22ae4778&amp;id=54a7906900',
   });
 
   function mailchimpCallback(resp) {
-    if(resp.result === 'success') {
+    if (resp.result === 'success') {
       $('.notify').find('.form').addClass('is-hidden');
       $('.notify').find('.subscription-ok').addClass('is-visible');
-      setTimeout(function() {
+      setTimeout(function () {
         // Done Functions
-        $('.notify').find('.subscription-ok').removeClass('is-visible');
-        $('.notify').find('.form').delay(300).removeClass('is-hidden');
-        $('.notify-form').trigger("reset");
+        $('.notify')
+          .find('.subscription-ok')
+          .removeClass('is-visible');
+        $('.notify')
+          .find('.form')
+          .delay(300)
+          .removeClass('is-hidden');
+        $('.notify-form').trigger('reset');
       }, 5000);
-    } else if(resp.result === 'error') {
+    } else if (resp.result === 'error') {
       $('.notify').find('.form').addClass('is-hidden');
       $('.notify').find('.subscription-error').addClass('is-visible');
-      setTimeout(function() {
+      setTimeout(function () {
         // Done Functions
-        $('.notify').find('.subscription-error').removeClass('is-visible');
-        $('.notify').find('.form').delay(300).removeClass('is-hidden');
-        $('.notify-form').trigger("reset");
+        $('.notify')
+          .find('.subscription-error')
+          .removeClass('is-visible');
+        $('.notify')
+          .find('.form')
+          .delay(300)
+          .removeClass('is-hidden');
+        $('.notify-form').trigger('reset');
       }, 5000);
     }
-  };
+  }
   // --------------------------------------------- //
   // Mailchimp Notify Form End
   // --------------------------------------------- //
@@ -205,26 +230,29 @@ $(function() {
   // --------------------------------------------- //
   // Contact Form Start
   // --------------------------------------------- //
-  $("#contact-form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
+  $('#contact-form').submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php', //Change
+      data: th.serialize(),
+    }).done(function () {
       $('.contact').find('.form').addClass('is-hidden');
       $('.contact').find('.reply-group').addClass('is-visible');
-			setTimeout(function() {
-				// Done Functions
+      setTimeout(function () {
+        // Done Functions
         $('.contact').find('.reply-group').removeClass('is-visible');
-        $('.contact').find('.form').delay(300).removeClass('is-hidden');
-				th.trigger("reset");
-			}, 5000);
-		});
-		return false;
-	});
+        $('.contact')
+          .find('.form')
+          .delay(300)
+          .removeClass('is-hidden');
+        th.trigger('reset');
+      }, 5000);
+    });
+    return false;
+  });
   // --------------------------------------------- //
   // Contact Form End
   // --------------------------------------------- //
-
 });
